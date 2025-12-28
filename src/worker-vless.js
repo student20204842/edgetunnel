943bffbf3595354bf2a7867c24a2be7452b264f0
student20204842 <20204842@fue.edu.eg>
2025-12-28 19:54:29 +0330

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a7a8979d-802d-4f01-9002-c9169dba3150';
 
 let proxyIP = '';
 
